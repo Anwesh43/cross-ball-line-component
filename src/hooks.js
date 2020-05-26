441f0b0d275075746907a0dcd55bc087f48c4fc6
Anwesh43 <anweshthecool0@gmail.com>
2020-05-26 10:36:35 +0530

    adding useStyle hook

--- src/hooks.js
+++ src/hooks.js
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react'
-
+import {sinify, divideScale} from './utils'
 export const useAnimatedScale = (scGap, delay) => {
     const [scale, setScale] = useState(0)
     const [animated, setAnimated] = useState(false)
@@ -33,12 +33,48 @@ export const useDimension = () => {
         }
         return () => {
             window.onresize = () => {
-                
+
             }
         }
     })
     return {
         w,
         h
+    }
+}
+
+export const useStyle = (w, h, scale) => {
+    const sf = sinify(scale)
+    const hSize = h / 3
+    const r = Math.min(w, h) / 10
+    const getY = (i) => {
+        const sf1 = divideScale(sf, 0, 2)
+        const si = 1 - 2 * i
+        return h / 2 - sf1 * si * hSize
+    }
+    const background = '#4CAF50'
+    return {
+        getBallSize(i) {
+            const y = getY(i) - 2 * r
+            const x = w / 2 - r
+            const position = 'absolute'
+            const left = `${x}px`
+            const top = `${y}px`
+            const width = `${2 * r}px`
+            const height = `${2 * r}px`
+            const borderRadius = '50%'
+            return {position, top, left, width, height, background, borderRadius}
+        },
+        getLineStyle(i) {
+            const lSize = Math.min(w, h) / 60
+            const y = getY(i)
+            const x = w / 2 - lSize / 2
+            const position = 'absolute'
+            const left = `${x}px`
+            const top = `${y}px`
+            const width = `${lSize}px`
+            const height = `${hSize * divideScale(sf, 0, 2)}px`
+            return {position, left, top, width, height, background}
+        }
     }
 }
